b13df7a62d5c40feaea43305cc796b55de0a2a75
alahmami <lahmami0020@gmail.com>
2025-02-21 20:11:02 +0100

    add fetch api

---
+++ frontend/social-network/api/fetchApi.jsx
@@ -0,0 +1,25 @@
+export async function fetchApi(method = "GET", endpoint, body = null, isFormData = false) {
+    const BaseURL = "http://localhost:8080";
+    const url = `${BaseURL}/${endpoint}`;
+    const headers = isFormData ? {} : { "Content-Type": "application/json" };
+    const options = {
+        method,
+        headers,
+        body: isFormData ? body : JSON.stringify(body),
+    };
+
+    if (method === "GET" || method === "DELETE") {
+        delete options.body;
+    }
+
+    try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+            throw new Error(`HTTP error Status: ${response.status}`);
+        }
+        return await response.json();
+    } catch (error) {
+        console.error("Fetch error:", error);
+        return { error: error.message };
+    }
+};
--- frontend/social-network/components/Posts/CreatePost.jsx
+++ frontend/social-network/components/Posts/CreatePost.jsx
@@ -1,17 +1,31 @@
+"use client";
+
+import { fetchApi } from "@/api/fetchApi";
+
 export function CreatePost() {
-    return (
-      <div className="newPost">
-        <textarea placeholder="Write something here..."></textarea>
-        <div className="imgPrivacyPost">
-          <label htmlFor="postImage" className="imageUp"><i className="fa-regular fa-image"></i> Image/GIF</label>
-          <input type="file" id="postImage" className="imageUpload" accept="image/*,image/gif" />
-          <select id="privacy" name="privacy">
-            <option value="public">Public</option>
-            <option value="almostPrivate">Almost Private (followers only)</option>
-            <option value="private">Private (selected followers)</option>
-          </select>
-        </div>
-        <button className="btn btnGreen">Post</button>
-      </div>
-    );
+  const hundleCreate = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target)
+    const response = await fetchApi("POST", "posts", formData, true)
+
+    console.log(response);
+    
   }
+  return (
+    <form className="newPost" encType="multipart/form-data" onSubmit={hundleCreate}>
+      <textarea placeholder="Write something here..." name="postContent"></textarea>
+      <div className="imgPrivacyPost">
+        <label htmlFor="postImage" className="imageUp">
+          <i className="fa-regular fa-image"></i> Image/GIF
+        </label>
+        <input type="file" id="postImage" className="imageUpload" accept="image/*,image/gif" name="postImage" />
+        <select id="privacy" name="privacy">
+          <option value="public">Public</option>
+          <option value="almostPrivate">Almost Private (followers only)</option>
+          <option value="private">Private (selected followers)</option>
+        </select>
+      </div>
+      <button type="submit" className="btn btnGreen">Post</button>
+    </form>
+  );
+}
