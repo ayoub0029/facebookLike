e6ee3cdf94e4212c1b162917ba399100ebfb6107
Ayoub Lahmami <37517528+ayoub0029@users.noreply.github.com>
2025-02-21 20:38:53 +0100

    Create fetchApi.jsx
    add fetch api function for all fetchs

---
+++ frontend/social-network/api/fetchApi.jsx
@@ -0,0 +1,34 @@
+/** -- al
+ * Fetch API helper for making HTTP requests.
+ * @param method - HTTP method (GET, POST, etc.).
+ * @param endpoint - API endpoint.
+ * @param body - Request body (for POST, PUT).
+ * @param isFormData - If true, sends FormData instead of JSON.
+ * @returns - JSON response or error object.
+ */
+
+export async function fetchApi(method = "GET", endpoint, body = null, isFormData = false) {
+    const BaseURL = "http://localhost:8080";
+    const url = `${BaseURL}/${endpoint}`;
+    const headers = isFormData ? {} : { "Content-Type": "application/json" };
+    const options = {
+        method,
+        headers,
+        body: isFormData ? body : JSON.stringify(body),
+    };
+
+    if (method === "GET" || method === "DELETE") {
+        delete options.body;
+    }
+
+    try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+            throw new Error(`HTTP error Status: ${response.status}`);
+        }
+        return await response.json();
+    } catch (error) {
+        console.error("Fetch error:", error);
+        return { error: error.message };
+    }
+};
