40eb5f5a48989efaaffa01e2896ebebb62a7471c
khirAbdelouahab <abdelohabk@gmail.com>
2025-03-14 19:07:25 +0100

    update is member state

--- backend/Groups/groups_data.go
+++ backend/Groups/groups_data.go
@@ -85,9 +85,18 @@ func getAllGroups(page int) []group_data {
 }
 
 func isMember(groupId, userId int) bool {
-	query := `SELECT COALESCE((SELECT  gm.user_id
-	 			FROM group_members gm
-				WHERE gm.group_id = ? AND gm.user_id = ?), 0) AS id`
+	query := `SELECT
+        COALESCE(
+            (
+                SELECT gm.user_id
+                FROM group_members gm
+                INNER JOIN groups g ON gm.group_id = g.id
+                WHERE g.owner_id = $1
+                OR (gm.status = 'accepted' AND gm.group_id = ? AND gm.user_id = ?)
+                LIMIT 1
+            ),
+            0
+        ) AS id;`
 	res, err := d.SelectOneRow(query, groupId, userId)
 	if err != nil {
 		return false
