a0c445ccd3356f1afaf6a18dfe90dc927e652620
zakaria salhi <106976277+zakariasalhi12@users.noreply.github.com>
2025-03-15 06:32:45 +0000

    Update page.jsx

--- frontend/social-network/app/(home)/notifications/page.jsx
+++ frontend/social-network/app/(home)/notifications/page.jsx
@@ -1,13 +1,22 @@
+"use client"
 import { Notification } from "@/components/notification/Notification";
-
+import { HomeProfile } from "@/components/homeLoggedUser";
+import { UsersFollowing } from "@/components/profile/users_follow";
 
 export default function Notifications() {
+
   return (
     <>
       <aside className="feed">
         <Notification />
       </aside>
-
+      <div className={"rightSidebar show"}>
+        <HomeProfile />
+        <div style={{ fontSize: "18px", fontWeight: "bold" }}>
+          Private Chat 
+        </div>
+        <UsersFollowing userID={window.userState.id} route={"/chat"} />
+      </div>
     </>
   )
 }
